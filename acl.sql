594124b3856f0aa36a2d46fc92d8d642c2c97bc0
chad <chad@mieasy.com>
2021-03-18 08:03:00 +0800

    sql文件

---
+++ acl.sql
@@ -0,0 +1,170 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: student_manage
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `acl_permission`
+--
+
+DROP TABLE IF EXISTS `acl_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acl_permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pid` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` int NOT NULL DEFAULT '0',
+  `permission_value` varchar(100) DEFAULT NULL,
+  `path` varchar(100) DEFAULT NULL,
+  `component` varchar(100) DEFAULT NULL,
+  `icon` varchar(45) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_permission`
+--
+
+LOCK TABLES `acl_permission` WRITE;
+/*!40000 ALTER TABLE `acl_permission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_role`
+--
+
+DROP TABLE IF EXISTS `acl_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acl_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(100) NOT NULL,
+  `role_code` varchar(45) DEFAULT NULL,
+  `remark` varchar(200) DEFAULT NULL,
+  `is_deleted` int NOT NULL DEFAULT '0',
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_role`
+--
+
+LOCK TABLES `acl_role` WRITE;
+/*!40000 ALTER TABLE `acl_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_role_permission`
+--
+
+DROP TABLE IF EXISTS `acl_role_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acl_role_permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `permission_id` int NOT NULL,
+  `is_deleted` int NOT NULL DEFAULT '0',
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_role_permission`
+--
+
+LOCK TABLES `acl_role_permission` WRITE;
+/*!40000 ALTER TABLE `acl_role_permission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl_role_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_user`
+--
+
+DROP TABLE IF EXISTS `acl_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acl_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nick_name` varchar(100) NOT NULL,
+  `sale` varchar(100) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `is_deleted` int NOT NULL DEFAULT '0',
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_user`
+--
+
+LOCK TABLES `acl_user` WRITE;
+/*!40000 ALTER TABLE `acl_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_user_role`
+--
+
+DROP TABLE IF EXISTS `acl_user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acl_user_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `is_deleted` int NOT NULL DEFAULT '0',
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_user_role`
+--
+
+LOCK TABLES `acl_user_role` WRITE;
+/*!40000 ALTER TABLE `acl_user_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl_user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-28  8:07:53
